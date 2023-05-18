d568dc9a92bdfee00d6b4b9c181890601b3f3ff2
Guillaume David <guillaumedaavidpro@gmail.com>
2023-05-18 15:06:49 +0200

    ajout commentaires

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import './Chat.css'
 
 function Chat() {
+
+    // variables d'états qui stockent le message envoyé, le nombre de messages envoyés et le dernier message combiné à la réponse du bot. 
     const [msg, setMsg] = useState('');
     const [countMsg, setCountMsg]= useState(0);
     const [newMsg, setNewMsg]= useState({'me': [], 'bot': []})
   
-
+    // fonction qui incrémente le nombre de messages et réinitialise le dernier message.
     function handleClick(e){
         e.preventDefault();
         setCountMsg(countMsg + 1);
         setMsg('');
     }
 
+    // met à jour la variable msg à chaque changement
     function handleChange(e){
         setMsg(e.target.value);
     }
